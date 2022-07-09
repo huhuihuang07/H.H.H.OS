13369bc08f352e46e9bab32c77c830caecac16dc
huihuanghu <18016511121@163.com>
2022-07-09 16:13:40 +0800

    [update]:修改Makefile生成自动依赖

---
+++ type.h
@@ -0,0 +1,22 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+#ifndef nullptr
+#define nullptr (void* (0))
+#endif
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+typedef unsigned char u8;
+
+typedef unsigned short u16;
+
+typedef unsigned int u32;
+
+typedef unsigned long long u64;
+
+typedef unsigned int size_t;
+
+#endif //!TYPE_H
