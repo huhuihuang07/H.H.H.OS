2ce1d7c6d82dcb147423a9ac6b282c972b6cda10
huihuanghu <18016511121@163.com>
2022-10-12 00:32:41 +0800

    [update]:系统调用时保存用到的寄存器

--- syscall.asm
+++ syscall.asm
@@ -8,12 +8,22 @@ SysCall:
 	push ebp
 	mov ebp, esp
 
+	push eax
+	push ebx
+	push ecx
+	push edx
+
 	mov edx, dword [ebp + 20]
 	mov ecx, dword [ebp + 16]
 	mov ebx, dword [ebp + 12]
 	mov eax, dword [ebp +  8]
 
 	int 0x80
+
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
 
 	leave
 	ret
